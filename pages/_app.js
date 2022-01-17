bb93d187b61fb6543a2150b9cba52310d3b546ae
Neh Joshi <nehjoshi5@gmail.com>
2022-01-17 15:10:09 +0530

    Added head and meta tags

--- pages/_app.js
+++ pages/_app.js
@@ -5,6 +5,8 @@ import Navigation from '../components/Navigation';
 import initFirebase from '../Firebase/Init';
 import "../styles/clear.css";
 import Footer from '../components/Footer';
+import Head from 'next/head';
+import Logo from "../static/owasp-logo.png";
 function MyApp({ Component, pageProps }) {
 
   const [user, setUser] = useState(null);
@@ -33,6 +35,16 @@ function MyApp({ Component, pageProps }) {
         setUser,
       }}
     >
+      <Head>
+        <title>OWASP VITCC Recruitments 2022-23</title>
+        <meta name="author" content="Hemang Ahuja" />
+      </Head>
+      <Head>
+        <meta name="description" content="The official portal for OWASP VITCC's recruitments 2022-23" />
+      </Head>
+      <Head>
+      <link rel="icon" href="/static/owasp-logo.png" />
+      </Head>
       <Navigation isAdmin={user && user.email == "hemang.ahuja2020@vitstudent.ac.in"}></Navigation>
       <Component {...pageProps} />
       <Footer />
