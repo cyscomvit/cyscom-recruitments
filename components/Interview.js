7c3be4a065266d77df692ca62e594c7f0db7b41c
Neh Joshi <nehjoshi5@gmail.com>
2022-01-18 15:40:36 +0530

    Discord server link

--- components/Interview.js
+++ components/Interview.js
@@ -15,27 +15,33 @@ export default function Interview({ user }) {
         }
         console.log(user);
         getInterview();
-    },[]);
-   
+    }, []);
+
     const formatData = (interview) => {
-        if(!interview.interview){
+        if (!interview.interview) {
             return "Error";
         }
-        if(interview.interview.status == "not_assigned"){
+        if (interview.interview.status == "not_assigned") {
             return `Your interview details will be announced soon. Hang tight!`;
         }
-        return `Your interview is scheduled on ${interview.interview.date} at ${interview.interview.time }`;
+        return `Your interview is scheduled on ${interview.interview.date} at ${interview.interview.time}`;
     }
-    
+
     return (
         <div>
-            
+
             {
-                loading ? <Alert className={styles.alert} message="Loading..." type="info"></Alert> : interview.error ? <Alert className={styles.alert} message={interview.error} type="error"></Alert> : 
-                <>
-                <h1 className={styles.alert}>{formatData(interview)}</h1>
-                <CandidateForm data={interview}></CandidateForm>
-                </>
+                loading ? <Alert className={styles.alert} message="Loading..." type="info"></Alert> : interview.error ? <Alert className={styles.alert} message={interview.error} type="error"></Alert> :
+                    <>
+                        <h1 className={styles.alert}>{formatData(interview)}</h1>
+                        <CandidateForm data={interview}></CandidateForm>
+                        <p style={{ width: '85%', margin: '0 auto', marginTop: '20px', fontSize: '1.3rem', textAlign: 'center' }}>
+                            Thank you for registering.
+                            Your can check your interview schedule here on 21st Jan'22, Friday.
+                            Please do join our discord server <a href="https://discord.gg/rKqdRUDvSw">here</a>  and check the category "Recruitment 2022-23". Also do react to the message in #announcements of this category.
+                            Stay tuned!
+                        </p>
+                    </>
             }
         </div>
     )
