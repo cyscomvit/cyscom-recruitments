ad5ccea328d454db1606bfd99bfedb9d91e65948
Ezhil Shanmugham <ezhil930@gmail.com>
2023-08-23 22:10:10 +0530

    Minor fixes

--- components/Navigation.js
+++ components/Navigation.js
@@ -1,64 +1,120 @@
 import Link from 'next/link';
 import { useContext, useEffect, useState } from 'react';
 import AppContext from '../context/state';
-import styles from "../styles/global/global.module.css";
+import styles from '../styles/global/global.module.css';
 import Image from 'next/image';
 import Script from 'next/script';
-import logo from "../static/cyscom-logo.png";
+import logo from '../static/cyscom-logo.png';
 
 export default function Navigation({ isAdmin }) {
-
   const value = useContext(AppContext);
   const [open, setOpen] = useState(false);
 
-  const handleClick = e => {
+  const handleClick = (e) => {
     value.setPageState(e.key);
   };
-  useEffect(() => {
-
-  }, [value])
+  useEffect(() => {}, [value]);
   return (
     <>
       <nav className={styles.nav}>
-        <Script src="https://kit.fontawesome.com/fc236c3dc0.js" crossorigin="anonymous" />
+        <Script
+          src='https://kit.fontawesome.com/fc236c3dc0.js'
+          crossorigin='anonymous'
+        />
         <div className={styles.logo}>
-          <a href="https://cyscomvit.com" target="_blank" rel="noreferrer"><Image src={logo} width={60} height={60} alt="logo" /></a>
+          <a href='/'>
+            <Image src={logo} width={70} height={60} alt='logo' />
+          </a>
         </div>
         <ul className={styles.navOptions}>
-          <li className={styles.navItem}><Link href='/' style={{ color: '#fff' }}><a className={styles.link}>Home</a></Link></li>
-          <li className={styles.navItem}><Link href='/form' style={{ color: '#fff' }}><a className={styles.link}>Register</a></Link></li>
-          <li className={styles.navItem}><Link href='/interview' style={{ color: '#fff' }}><a className={styles.link}>Interview</a></Link></li>
-          <li className={styles.navItem}><Link href='/results' style={{ color: '#fff' }}><a className={styles.link}>Results</a></Link></li>
-          {isAdmin &&
+          <li className={styles.navItem}>
+            <Link href='/' style={{ color: '#fff' }}>
+              <a className={styles.link}>Home</a>
+            </Link>
+          </li>
+          <li className={styles.navItem}>
+            <Link href='/form' style={{ color: '#fff' }}>
+              <a className={styles.link}>Register</a>
+            </Link>
+          </li>
+          <li className={styles.navItem}>
+            <Link href='/interview' style={{ color: '#fff' }}>
+              <a className={styles.link}>Interview</a>
+            </Link>
+          </li>
+          <li className={styles.navItem}>
+            <Link href='/results' style={{ color: '#fff' }}>
+              <a className={styles.link}>Results</a>
+            </Link>
+          </li>
+          {isAdmin && (
             <>
-              <li className={styles.navItem}><Link href='/admin/forms' style={{ color: '#fff' }}><a className={styles.link}>Review Forms</a></Link></li>
-              <li className={styles.navItem}><Link href='/admin/publish' style={{ color: '#fff' }}><a className={styles.link}>Publish Results</a></Link></li>
+              <li className={styles.navItem}>
+                <Link href='/admin/forms' style={{ color: '#fff' }}>
+                  <a className={styles.link}>Review Forms</a>
+                </Link>
+              </li>
+              <li className={styles.navItem}>
+                <Link href='/admin/publish' style={{ color: '#fff' }}>
+                  <a className={styles.link}>Publish Results</a>
+                </Link>
+              </li>
             </>
-          }
+          )}
         </ul>
-        <div className={styles.burger}><i className="fas fa-bars" onClick={() => setOpen(true)} style={{ color: "#fff" }}></i></div>
+        <div className={styles.burger}>
+          <i
+            className='fas fa-bars'
+            onClick={() => setOpen(true)}
+            style={{ color: '#fff' }}
+          ></i>
+        </div>
       </nav>
-      {open &&
+      {open && (
         <div className={styles.sidebarOverlay}>
           <div className={styles.sidebar}>
             <div className={styles.closeIcon}>
-              <i className="fas fa-times" onClick={() => setOpen(false)}></i>
+              <i className='fas fa-times' onClick={() => setOpen(false)}></i>
             </div>
             <div className={styles.sidebarColumn}>
-              <Link href='/'><a style={{ color: "black" }} className={styles.link}>Home</a></Link>
-              <Link href='/form'><a style={{ color: "black" }} className={styles.link}>Register</a></Link>
-              <Link href='/interview'><a style={{ color: "black" }} className={styles.link}>Interview</a></Link>
-              <Link href='/results'><a style={{ color: "black" }} className={styles.link}>Results</a></Link>
-              {isAdmin &&
+              <Link href='/'>
+                <a style={{ color: 'black' }} className={styles.link}>
+                  Home
+                </a>
+              </Link>
+              <Link href='/form'>
+                <a style={{ color: 'black' }} className={styles.link}>
+                  Register
+                </a>
+              </Link>
+              <Link href='/interview'>
+                <a style={{ color: 'black' }} className={styles.link}>
+                  Interview
+                </a>
+              </Link>
+              <Link href='/results'>
+                <a style={{ color: 'black' }} className={styles.link}>
+                  Results
+                </a>
+              </Link>
+              {isAdmin && (
                 <>
-                  <Link href='/admin/forms'><a style={{ color: "black" }} className={styles.link}>Review Forms</a></Link>
-                  <Link href='/admin/publish'><a style={{ color: "black" }} className={styles.link}>Publish Results</a></Link>
+                  <Link href='/admin/forms'>
+                    <a style={{ color: 'black' }} className={styles.link}>
+                      Review Forms
+                    </a>
+                  </Link>
+                  <Link href='/admin/publish'>
+                    <a style={{ color: 'black' }} className={styles.link}>
+                      Publish Results
+                    </a>
+                  </Link>
                 </>
-              }
+              )}
             </div>
           </div>
         </div>
-      }
+      )}
     </>
   );
 }
--- pages/admin/publish.js
+++ pages/admin/publish.js
@@ -1,35 +1,50 @@
-import PubResultComp from '../../components/PublishResultsComponent'
-import { useContext , useEffect , useState } from 'react'
-import AppContext from '../../context/state'
-import updateFirestore from '../../Firebase/AddMembers'
-import getAllDocs from '../../Firebase/ReadAllUsers'
-import ViewSelected from '../../components/ViewSelected'
+import PubResultComp from '../../components/PublishResultsComponent';
+import { useContext, useEffect, useState } from 'react';
+import AppContext from '../../context/state';
+import updateFirestore from '../../Firebase/AddMembers';
+import getAllDocs from '../../Firebase/ReadAllUsers';
+import ViewSelected from '../../components/ViewSelected';
 export default function Admin() {
+  const [docs, setDocs] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-    const [docs, setDocs] = useState([])
-    const [loading, setLoading] = useState(true)
-   
-    useEffect(() => {
-        value.setPageState("publish")
-        const getData = async () => {
-            const Myids = await getAllDocs();
-            setDocs(Myids);
-            setLoading(false);
-        }
-        getData();
-    }, [])
-    const value = useContext(AppContext)
-    
-    const handlePublish = (data) => {
-        updateFirestore(data)
-    }
-    return (
-        <div style={{ 
-            background: "linear-gradient( rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6) ), url(/static/bg.jpg)", 
-            backgroundSize: 'cover', 
-            minHeight: '92.5vh',
-            paddingTop: '70px' }} >
-            {loading ? <div>Loading...</div> : value.state.user && value.state.isAdmin ? <><PubResultComp onSubmit = {handlePublish} allEmails={docs}></PubResultComp><ViewSelected></ViewSelected></> : <div>Not Found</div>}
-        </div>
-    )
+  useEffect(() => {
+    value.setPageState('publish');
+    const getData = async () => {
+      const Myids = await getAllDocs();
+      setDocs(Myids);
+      setLoading(false);
+    };
+    getData();
+  }, []);
+  const value = useContext(AppContext);
+
+  const handlePublish = (data) => {
+    updateFirestore(data);
+  };
+  return (
+    <div
+      style={{
+        background:
+          'linear-gradient( rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6) ), url(/static/bg.jpg)',
+        backgroundSize: 'cover',
+        minHeight: '92.5vh',
+        paddingTop: '70px',
+      }}
+    >
+      {loading ? (
+        <div>Loading...</div>
+      ) : value.state.user && value.state.isAdmin ? (
+        <>
+          <PubResultComp
+            onSubmit={handlePublish}
+            allEmails={docs}
+          ></PubResultComp>
+          <ViewSelected></ViewSelected>
+        </>
+      ) : (
+        <div>Not Found</div>
+      )}
+    </div>
+  );
 }
