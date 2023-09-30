dd68735cb66adb142a0f9539566d45c5a41e669e
Ezhil Shanmugham <ezhil930@gmail.com>
2023-10-01 02:50:29 +0530

    Minor Fixes

--- components/AssignInterview.js
+++ components/AssignInterview.js
@@ -2,137 +2,157 @@ import { Form, Select, Button } from 'antd';
 import { useState } from 'react';
 
 export default function AssignInterview({ handleSubmit }) {
-  const layout = {
-    labelCol: { span: 4 },
-    wrapperCol: { span: 8 },
-  };
+	const layout = {
+		labelCol: { span: 4 },
+		wrapperCol: { span: 8 },
+	};
 
-  const [form] = Form.useForm();
-  const [day, setDay] = useState(21);
+	const [form] = Form.useForm();
+	const [day, setDay] = useState(21);
 
-  const generateTimeOptions = (startHour, endHour, intervalMinutes) => {
-    const options = [];
-    const startTime = new Date(`1970-01-01 ${startHour}`);
-    const endTime = new Date(`1970-01-01 ${endHour}`);
+	const generateTimeOptions = (startHour, endHour, intervalMinutes) => {
+		const options = [];
+		const startTime = new Date(`1970-01-01 ${startHour}`);
+		const endTime = new Date(`1970-01-01 ${endHour}`);
 
-    while (startTime <= endTime) {
-      const formattedTime = startTime.toLocaleTimeString([], {
-        hour: 'numeric',
-        minute: '2-digit',
-        hour12: true,
-      });
+		while (startTime <= endTime) {
+			const formattedTime = startTime.toLocaleTimeString([], {
+				hour: 'numeric',
+				minute: '2-digit',
+				hour12: true,
+			});
 
-      options.push(
-        <Select.Option
-          key={formattedTime}
-          style={{
-            background: '#28126C',
-          }}
-          value={
-            formattedTime.split(' ')[0] +
-            ' ' +
-            formattedTime.split(' ')[1].toUpperCase()
-          }
-        >
-          {formattedTime.split(' ')[0] +
-            ' ' +
-            formattedTime.split(' ')[1].toUpperCase()}
-        </Select.Option>
-      );
+			options.push(
+				<Select.Option
+					key={formattedTime}
+					style={{
+						background: '#28126C',
+					}}
+					value={
+						formattedTime.split(' ')[0] +
+						' ' +
+						formattedTime.split(' ')[1].toUpperCase()
+					}
+				>
+					{formattedTime.split(' ')[0] +
+						' ' +
+						formattedTime.split(' ')[1].toUpperCase()}
+				</Select.Option>
+			);
 
-      startTime.setMinutes(startTime.getMinutes() + intervalMinutes);
-    }
+			startTime.setMinutes(startTime.getMinutes() + intervalMinutes);
+		}
 
-    return options;
-  };
+		return options;
+	};
 
-  //   August 27
-  const day1Slot1 = generateTimeOptions('10:00', '11:50', 10);
-  const day1Slot2 = generateTimeOptions('13:30', '15:20', 10);
-  const day1Slot3 = generateTimeOptions('15:50', '17:20', 10);
-  const day1Slot4 = generateTimeOptions('18:30', '21:20', 10);
+	// Create date and slot options for each day
+	//   August 27
+	const day1Slot1 = generateTimeOptions('10:00', '11:50', 10);
+	const day1Slot2 = generateTimeOptions('13:30', '15:20', 10);
+	const day1Slot3 = generateTimeOptions('15:50', '17:20', 10);
+	const day1Slot4 = generateTimeOptions('18:30', '21:20', 10);
 
-  //   August 28
-  const day2Slot1 = generateTimeOptions('11:50', '13:20', 10);
-  const day2Slot2 = generateTimeOptions('18:00', '19:20', 10);
+	//   August 28
+	const day2Slot1 = generateTimeOptions('11:50', '13:20', 10);
+	const day2Slot2 = generateTimeOptions('18:00', '19:20', 10);
 
-  return (
-    <div>
-      <Form form={form} name='basic' onFinish={handleSubmit} {...layout}>
-        <Form.Item
-          name='dateAssign'
-          label='Date Assign'
-          rules={[
-            { required: true, message: 'Please given an assigned date!' },
-          ]}
-        >
-          <Select onChange={(e) => setDay(e)}>
-            <Select.Option
-              style={{
-                background: '#28126C',
-              }}
-              value='27'
-            >
-              27 August, 2023
-            </Select.Option>
-            <Select.Option
-              style={{
-                background: '#28126C',
-              }}
-              value='28'
-            >
-              28 August, 2023
-            </Select.Option>
-            <Select.Option
-              style={{
-                background: '#28126C',
-              }}
-              value='31' // 31st August
-            >
-              31 August, 2023
-            </Select.Option>
-            <Select.Option
-              style={{
-                background: '#28126C',
-              }}
-              value='1' // 1st September
-            >
-              1 September, 2023
-            </Select.Option>
-          </Select>
-        </Form.Item>
-        <Form.Item
-          name='timeAssign'
-          label='Time Assign'
-          rules={[{ required: true, message: 'Please give an assigned time!' }]}
-        >
-          {day === '27' ? (
-            <Select>
-              {day1Slot1}
-              {day1Slot2}
-              {day1Slot3}
-              {day1Slot4}
-            </Select>
-          ) : (
-            <Select>
-              {day2Slot1}
-              {day2Slot2}
-            </Select>
-          )}
-        </Form.Item>
-        <Form.Item>
-          <Button
-            style={{ margin: '2%' }}
-            type='primary'
-            htmlType='submit'
-            onClick={() => {
-              form.validateFields();
-            }}
-          >
-            Submit
-          </Button>
-        </Form.Item>
-      </Form>
-    </div>
-  );
+	//   August 31
+	const day3Slot1 = generateTimeOptions('11:50', '13:20', 10);
+	const day3Slot2 = generateTimeOptions('18:00', '19:20', 10);
+
+	// September 1
+	const day4Slot1 = generateTimeOptions('11:50', '13:20', 10);
+	const day4Slot2 = generateTimeOptions('18:00', '19:20', 10);
+
+	return (
+		<div>
+			<Form form={form} name='basic' onFinish={handleSubmit} {...layout}>
+				<Form.Item
+					name='dateAssign'
+					label='Date Assign'
+					rules={[
+						{ required: true, message: 'Please given an assigned date!' },
+					]}
+				>
+					<Select onChange={(e) => setDay(e)}>
+						<Select.Option
+							style={{
+								background: '#28126C',
+							}}
+							value='27'
+						>
+							27 August, 2023
+						</Select.Option>
+						<Select.Option
+							style={{
+								background: '#28126C',
+							}}
+							value='28'
+						>
+							28 August, 2023
+						</Select.Option>
+						<Select.Option
+							style={{
+								background: '#28126C',
+							}}
+							value='31' // 31st August
+						>
+							31 August, 2023
+						</Select.Option>
+						<Select.Option
+							style={{
+								background: '#28126C',
+							}}
+							value='1' // 1st September
+						>
+							1 September, 2023
+						</Select.Option>
+					</Select>
+				</Form.Item>
+				<Form.Item
+					name='timeAssign'
+					label='Time Assign'
+					rules={[{ required: true, message: 'Please give an assigned time!' }]}
+				>
+					{/* Generate time options based on the day selected */}
+					{day === '27' ? (
+						<Select>
+							{day1Slot1}
+							{day1Slot2}
+							{day1Slot3}
+							{day1Slot4}
+						</Select>
+					) : day === '28' ? (
+						<Select>
+							{day2Slot1}
+							{day2Slot2}
+						</Select>
+					) : day === '31' ? (
+						<Select>
+							{day3Slot1}
+							{day3Slot2}
+						</Select>
+					) : (
+						<Select>
+							{day4Slot1}
+							{day4Slot2}
+						</Select>
+					)}
+				</Form.Item>
+				<Form.Item>
+					<Button
+						style={{ margin: '2%' }}
+						type='primary'
+						htmlType='submit'
+						onClick={() => {
+							form.validateFields();
+						}}
+					>
+						Submit
+					</Button>
+				</Form.Item>
+			</Form>
+		</div>
+	);
 }
--- components/Interview.js
+++ components/Interview.js
@@ -5,62 +5,63 @@ import styles from '../styles/global/global.module.css';
 import readFromFirestore from '../Firebase/ReadUser';
 
 export default function Interview({ user }) {
-  const [interview, setInterview] = useState(null);
-  const [loading, setLoading] = useState(true);
-  useEffect(() => {
-    const getInterview = async () => {
-      const myinterview = await readFromFirestore(user.email);
-      setInterview(myinterview);
-      setLoading(false);
-    };
-    getInterview();
-  }, []);
+	const [interview, setInterview] = useState(null);
+	const [loading, setLoading] = useState(true);
+	useEffect(() => {
+		const getInterview = async () => {
+			const myinterview = await readFromFirestore(user.email);
+			setInterview(myinterview);
+			setLoading(false);
+		};
+		getInterview();
+	}, []);
 
-  const formatData = (interview) => {
-    if (!interview.interview) {
-      return 'Error';
-    }
-    if (interview.interview.status == 'not_assigned') {
-      return `Your interview details will be announced soon. Hang tight!`;
-    }
-    return `Your interview is scheduled on ${interview.interview.date} at ${interview.interview.time}`;
-  };
+	const formatData = (interview) => {
+		if (!interview.interview) {
+			return 'Error';
+		}
+		if (interview.interview.status == 'not_assigned') {
+			return `Your interview details will be announced soon. Hang tight!`;
+		}
+		return `Your interview is scheduled on ${interview.interview.date} at ${interview.interview.time}`;
+	};
 
-  return (
-    <div>
-      {loading ? (
-        <Alert
-          className={styles.alert}
-          message='Loading...'
-          type='info'
-        ></Alert>
-      ) : interview.error ? (
-        <Alert
-          className={styles.alert}
-          message={interview.error}
-          type='error'
-        ></Alert>
-      ) : (
-        <>
-          <h1 className={styles.alert}>{formatData(interview)}</h1>
-          {/*<CandidateForm data={interview}></CandidateForm>*/}
-          <p
-            style={{
-              width: '85%',
-              margin: '0 auto',
-              marginTop: '20px',
-              fontSize: '1.3rem',
-              textAlign: 'center',
-            }}
-          >
-            Thank you for registering. For any queries, join our{' '}
-            <a href='https://chat.whatsapp.com/I6ihKqtsz7kFcDH8DcOBW3'>
-              WhatsApp Help desk (Click here)
-            </a>
-            Stay tuned!
-          </p>
-        </>
-      )}
-    </div>
-  );
+	return (
+		<div>
+			{loading ? (
+				<Alert
+					className={styles.alert}
+					message='Loading...'
+					type='info'
+				></Alert>
+			) : interview.error ? (
+				<Alert
+					className={styles.alert}
+					message={interview.error}
+					type='error'
+				></Alert>
+			) : (
+				<>
+					<h1 className={styles.alert}>{formatData(interview)}</h1>
+					{/* Uncomment this if you want to show the details table */}
+					{/* <CandidateForm data={interview}></CandidateForm> */}
+					<p
+						style={{
+							width: '85%',
+							margin: '0 auto',
+							marginTop: '20px',
+							fontSize: '1.3rem',
+							textAlign: 'center',
+						}}
+					>
+						Thank you for registering. For any queries, join our{' '}
+						<a href='https://chat.whatsapp.com/I6ihKqtsz7kFcDH8DcOBW3'>
+							WhatsApp Help desk (Click here)
+						</a>
+						Stay tuned!
+					</p>
+				</>
+			)}
+		</div>
+	);
 }
