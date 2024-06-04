5fdc182b2b72e55f18d07f1b57b92f6f03d4d9ca
Ezhil Shanmugham <ezhil930@gmail.com>
2024-06-04 22:44:33 +0530

    chore: 2023 recruitments (#6)

--- components/PublishResultsComponent.js
+++ components/PublishResultsComponent.js
@@ -4,10 +4,10 @@ const { Option } = Select;
 import { useState } from 'react';
 import unPublish from '../Firebase/UnPublish';
 import finalPublish from '../Firebase/FinalPublish';
-const PubResultComp = ({onSubmit , allEmails}) => {
+const PubResultComp = ({ onSubmit, allEmails }) => {
   const [submitting, setSubmitting] = useState(false);
 
-  const onFinish = values => {
+  const onFinish = (values) => {
     setSubmitting(true);
     onSubmit(values.users);
     setTimeout(() => {
@@ -16,76 +16,119 @@ const PubResultComp = ({onSubmit , allEmails}) => {
   };
 
   return (
-      <div style={{paddingTop:"5rem"}}>
-      {submitting ? <Alert message = {"Publishing Results.. Click Refresh to see changes"} type='info'></Alert> : null}
+    <div style={{ paddingTop: '5rem' }}>
+      {submitting ? (
+        <Alert
+          message={'Publishing Results.. Click Refresh to see changes'}
+          type='info'
+        ></Alert>
+      ) : null}
       <h1>Publish Results here</h1>
-    <Form name="dynamic_form_nest_item" onFinish={onFinish} autoComplete="off">
-      <Form.List name="users">
-        {(fields, { add, remove }) => (
-          <>
-            {fields.map(({ key, name, ...restField }) => (
-              <Space key={key} style={{ display: 'flex', marginBottom: 8 }} align="baseline">
-                <Form.Item
-                  {...restField}
-                  name={[name, 'email']}
-                  rules={[{ required: true, message: 'Missing first name' }]}
+      <Form
+        name='dynamic_form_nest_item'
+        onFinish={onFinish}
+        autoComplete='off'
+      >
+        <Form.List name='users'>
+          {(fields, { add, remove }) => (
+            <>
+              {fields.map(({ key, name, ...restField }) => (
+                <Space
+                  key={key}
+                  style={{ display: 'flex', marginBottom: 8 }}
+                  align='baseline'
                 >
-                  <Select style={{"width": "30rem"}} mode='multiple' placeholder = 'Email'>
-                    {allEmails.map((email) => {
-                        return <Option key={email} value={email}>{email}</Option>
-                    })}
+                  <Form.Item
+                    {...restField}
+                    name={[name, 'email']}
+                    rules={[{ required: true, message: 'Missing first name' }]}
+                  >
+                    <Select
+                      style={{ width: '30rem' }}
+                      mode='multiple'
+                      placeholder='Email'
+                    >
+                      {allEmails.map((email) => {
+                        return (
+                          <Option key={email} value={email}>
+                            {email}
+                          </Option>
+                        );
+                      })}
                     </Select>
-                </Form.Item>
-               <Form.Item
-                    name = {[name, 'departmentsSelected']}
+                  </Form.Item>
+                  <Form.Item
+                    name={[name, 'departmentsSelected']}
                     {...restField}
                     rules={[{ required: true, message: 'Missing department' }]}
-               >
-               <Select style={{width:"30rem"}} mode = 'multiple' placeholder="Departments">
-                    <Option value="Development (Web-Dev and Projects)">Development (Web-Dev and Projects)</Option>
-                    <Option value="CTF">CTF</Option>
-                    <Option value="Design">Design</Option>
-                    <Option value="Event Management">Event Management</Option>
-                    <Option value="Content">Content</Option>
-                    <Option value="Social Media">Social Media</Option>
-                    <Option value="Sponsorship and Finance">Sponsorship and Finance</Option>
-               </Select>
-               </Form.Item>
-                <MinusCircleOutlined onClick={() => remove(name)} />
-              </Space>
-            ))}
-            <Form.Item>
-              <Button type="dashed" onClick={() => add()} block icon={<PlusOutlined />}>
-                Add Member
-              </Button>
-            </Form.Item>
-          </>
-        )}
-      </Form.List>
-      <Form.Item>
-        <Button type="primary" htmlType="submit">
-          Submit
+                  >
+                    <Select
+                      style={{ width: '30rem' }}
+                      mode='multiple'
+                      placeholder='Departments'
+                    >
+                      <Option value='Development (Web-Dev and Open Source Projects)'>
+                        Development (Web-Dev and Open Source Projects)
+                      </Option>
+                      <Option value='Technical (CTF and Projects)'>
+                        Technical (CTF and Projects)
+                      </Option>
+                      <Option value='Design'>Design</Option>
+                      <Option value='Event Management'>Event Management</Option>
+                      <Option value='Content'>Content</Option>
+                      <Option value='Social Media'>Social Media</Option>
+                    </Select>
+                  </Form.Item>
+                  <MinusCircleOutlined onClick={() => remove(name)} />
+                </Space>
+              ))}
+              <Form.Item>
+                <Button
+                  type='dashed'
+                  onClick={() => add()}
+                  block
+                  icon={<PlusOutlined />}
+                >
+                  Add Member
+                </Button>
+              </Form.Item>
+            </>
+          )}
+        </Form.List>
+        <Form.Item>
+          <Button type='primary' htmlType='submit'>
+            Submit
+          </Button>
+        </Form.Item>
+      </Form>
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          columnGap: '1rem',
+        }}
+      >
+        <Button
+          style={{
+            width: '10rem',
+          }}
+          type='primary'
+          onClick={unPublish}
+        >
+          UnPublish
         </Button>
-      </Form.Item>
-    </Form>
-        <div style={{
-            display: 'flex',
-            flexDirection: "column",
-            columnGap: "1rem",
-            
-        }}>
-        <Button style={{
-          width: "10rem",
-        }} type="primary" onClick={unPublish}>
-      UnPublish
-    </Button>
-    <Button style={{
-      marginTop: "1rem",
-      width: "40rem",
-    }} type="primary" onClick={finalPublish}>
-      Final Publish ! ! !  Caution that this will reject all candidates who have not been selected already.
-    </Button>
-        </div>
+        <Button
+          style={{
+            marginTop: '1rem',
+            width: '40rem',
+          }}
+          type='primary'
+          onClick={finalPublish}
+        >
+          Final Publish ! ! ! Caution that this will reject all candidates who
+          have not been selected already.
+        </Button>
+      </div>
     </div>
   );
 };
