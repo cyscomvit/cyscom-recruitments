a651c04a07713f5f42ce956b10e9741b6b7cd9f6
Ezhil Shanmugham <ezhil930@gmail.com>
2024-06-04 22:41:27 +0530

    chore: 2023 recruitments

--- components/AssignInterview.js
+++ components/AssignInterview.js
@@ -1,159 +1,158 @@
-import { Form, Select, Button } from "antd";
+import { Form, Select, Button } from 'antd';
 import { useState } from 'react';
 
 export default function AssignInterview({ handleSubmit }) {
-    const layout = {
-        labelCol: { span: 4 },
-        wrapperCol: { span: 8 },
+	const layout = {
+		labelCol: { span: 4 },
+		wrapperCol: { span: 8 },
+	};
 
-    };
+	const [form] = Form.useForm();
+	const [day, setDay] = useState(21);
 
-    const [form] = Form.useForm();
-    const [day, setDay] = useState(21);
-    return (
-        <div>
-            <Form form={form} name="basic" onFinish={handleSubmit} {...layout}>
-                <Form.Item
-                    name="dateAssign"
-                    label="Date Assign"
-                    rules={[
-                        { required: true, message: "Please given an assigned date!" },
-                    ]}
-                >
-                    <Select onChange={e => setDay(e)}>
-                        <Select.Option
-                            style={{
-                                background: "#28126C",
-                            }}
-                            value="05"
-                        >
-                            05
-                        </Select.Option>
-                        <Select.Option
-                            style={{
-                                background: "#28126C",
-                            }}
-                            value="06"
-                        >
-                            06
-                        </Select.Option>
-                    </Select>
-                </Form.Item>
-                <Form.Item
-                    name="timeAssign"
-                    label="Time Assign"
-                    rules={[
-                        { required: true, message: "Please give an assigned time!" },
-                    ]}
-                >
-                    {day === '26' ?
-                        <Select>
-                            <Select.Option style={{
-                                background: "#28126C"
-                            }} value="19:00">19:00</Select.Option>
-                            <Select.Option style={{
-                                background: "#28126C"
-                            }} value="19:10">19:10</Select.Option>
-                            <Select.Option style={{
-                                background: "#28126C"
-                            }} value="19:20">19:20</Select.Option>
-                            <Select.Option style={{
-                                background: "#28126C"
-                            }} value="19:30">19:30</Select.Option>
-                            <Select.Option style={{
-                                background: "#28126C"
-                            }} value="19:40">19:40</Select.Option>
-                            <Select.Option style={{
-                                background: "#28126C"
-                            }} value="19:50">19:50</Select.Option>
-                            <Select.Option style={{
-                                background: "#28126C"
-                            }} value="20:00">20:00</Select.Option>
-                            <Select.Option style={{
-                                background: "#28126C"
-                            }} value="20:10">20:10</Select.Option>
-                            <Select.Option style={{
-                                background: "#28126C"
-                            }} value="20:20">20:20</Select.Option>
-                            <Select.Option style={{
-                                background: "#28126C"
-                            }} value="20:30">20:30</Select.Option>
-                            <Select.Option style={{
-                                background: "#28126C"
-                            }} value="20:40">20:40</Select.Option>
-                            <Select.Option style={{
-                                background: "#28126C"
-                            }} value="20:50">20:50</Select.Option>
-                            <Select.Option style={{
-                                background: "#28126C"
-                            }} value="21:00">21:00</Select.Option>
-                        </Select>
-                        :
-                        <Select>
-                            <Select.Option style={{
-                                background: "#28126C"
-                            }} value="10:30">10:30</Select.Option>
-                            <Select.Option style={{
-                                background: "#28126C"
-                            }} value="10:40">10:40</Select.Option>
-                            <Select.Option style={{
-                                background: "#28126C"
-                            }} value="10:50">10:50</Select.Option>
-                            <Select.Option style={{
-                                background: "#28126C"
-                            }} value="11:00">11:00</Select.Option>
-                            <Select.Option style={{
-                                background: "#28126C"
-                            }} value="11:10">11:10</Select.Option>
-                            <Select.Option style={{
-                                background: "#28126C"
-                            }} value="11:20">11:20</Select.Option>
-                            <Select.Option style={{
-                                background: "#28126C"
-                            }} value="11:30">11:30</Select.Option>
-                            <Select.Option style={{
-                                background: "#28126C"
-                            }} value="11:40">11:40</Select.Option>
-                            <Select.Option style={{
-                                background: "#28126C"
-                            }} value="11:50">11:50</Select.Option>
-                            <Select.Option style={{
-                                background: "#28126C"
-                            }} value="12:00">12:00</Select.Option>
-                            <Select.Option style={{
-                                background: "#28126C"
-                            }} value="12:10">12:10</Select.Option>
-                            <Select.Option style={{
-                                background: "#28126C"
-                            }} value="12:20">12:20</Select.Option>
-                            <Select.Option style={{
-                                background: "#28126C"
-                            }} value="12:30">12:30</Select.Option>
-                            <Select.Option style={{
-                                background: "#28126C"
-                            }} value="12:40">12:40</Select.Option>
-                            <Select.Option style={{
-                                background: "#28126C"
-                            }} value="12:50">12:50</Select.Option>  
-                            <Select.Option style={{
-                                background: "#28126C"
-                            }} value="13:00">13:00</Select.Option>
-                        </Select>
-                    }
-                </Form.Item>
-                <Form.Item>
-                    <Button
-                        style={{ margin: "2%" }}
-                        type="primary"
-                        htmlType="submit"
-                        onClick={() => {
-                            form.validateFields();
-                        }}
-                    >
-                        Submit
-                    </Button>
-                </Form.Item>
-            </Form>
-        </div>
-    );
+	const generateTimeOptions = (startHour, endHour, intervalMinutes) => {
+		const options = [];
+		const startTime = new Date(`1970-01-01 ${startHour}`);
+		const endTime = new Date(`1970-01-01 ${endHour}`);
+
+		while (startTime <= endTime) {
+			const formattedTime = startTime.toLocaleTimeString([], {
+				hour: 'numeric',
+				minute: '2-digit',
+				hour12: true,
+			});
+
+			options.push(
+				<Select.Option
+					key={formattedTime}
+					style={{
+						background: '#28126C',
+					}}
+					value={
+						formattedTime.split(' ')[0] +
+						' ' +
+						formattedTime.split(' ')[1].toUpperCase()
+					}
+				>
+					{formattedTime.split(' ')[0] +
+						' ' +
+						formattedTime.split(' ')[1].toUpperCase()}
+				</Select.Option>
+			);
+
+			startTime.setMinutes(startTime.getMinutes() + intervalMinutes);
+		}
+
+		return options;
+	};
+
+	// Create date and slot options for each day
+	//   August 27
+	const day1Slot1 = generateTimeOptions('10:00', '11:50', 10);
+	const day1Slot2 = generateTimeOptions('13:30', '15:20', 10);
+	const day1Slot3 = generateTimeOptions('15:50', '17:20', 10);
+	const day1Slot4 = generateTimeOptions('18:30', '21:20', 10);
+
+	//   August 28
+	const day2Slot1 = generateTimeOptions('11:50', '13:20', 10);
+	const day2Slot2 = generateTimeOptions('18:00', '19:20', 10);
+
+	//   August 31
+	const day3Slot1 = generateTimeOptions('11:50', '13:20', 10);
+	const day3Slot2 = generateTimeOptions('18:00', '19:20', 10);
+
+	// September 1
+	const day4Slot1 = generateTimeOptions('11:50', '13:20', 10);
+	const day4Slot2 = generateTimeOptions('18:00', '19:20', 10);
+
+	return (
+		<div>
+			<Form form={form} name='basic' onFinish={handleSubmit} {...layout}>
+				<Form.Item
+					name='dateAssign'
+					label='Date Assign'
+					rules={[
+						{ required: true, message: 'Please given an assigned date!' },
+					]}
+				>
+					<Select onChange={(e) => setDay(e)}>
+						<Select.Option
+							style={{
+								background: '#28126C',
+							}}
+							value='27'
+						>
+							27 August, 2023
+						</Select.Option>
+						<Select.Option
+							style={{
+								background: '#28126C',
+							}}
+							value='28'
+						>
+							28 August, 2023
+						</Select.Option>
+						<Select.Option
+							style={{
+								background: '#28126C',
+							}}
+							value='31' // 31st August
+						>
+							31 August, 2023
+						</Select.Option>
+						<Select.Option
+							style={{
+								background: '#28126C',
+							}}
+							value='1' // 1st September
+						>
+							1 September, 2023
+						</Select.Option>
+					</Select>
+				</Form.Item>
+				<Form.Item
+					name='timeAssign'
+					label='Time Assign'
+					rules={[{ required: true, message: 'Please give an assigned time!' }]}
+				>
+					{/* Generate time options based on the day selected */}
+					{day === '27' ? (
+						<Select>
+							{day1Slot1}
+							{day1Slot2}
+							{day1Slot3}
+							{day1Slot4}
+						</Select>
+					) : day === '28' ? (
+						<Select>
+							{day2Slot1}
+							{day2Slot2}
+						</Select>
+					) : day === '31' ? (
+						<Select>
+							{day3Slot1}
+							{day3Slot2}
+						</Select>
+					) : (
+						<Select>
+							{day4Slot1}
+							{day4Slot2}
+						</Select>
+					)}
+				</Form.Item>
+				<Form.Item>
+					<Button
+						style={{ margin: '2%' }}
+						type='primary'
+						htmlType='submit'
+						onClick={() => {
+							form.validateFields();
+						}}
+					>
+						Submit
+					</Button>
+				</Form.Item>
+			</Form>
+		</div>
+	);
 }
--- pages/about.js
+++ pages/about.js
@@ -1,37 +1,46 @@
-import React from 'react'
-import { useContext , useEffect } from 'react'
-import AppContext from '../context/state'
+import React from 'react';
+import { useContext, useEffect } from 'react';
+import AppContext from '../context/state';
 import PersonalForm from '../components/PersonalForm';
-import FormSteps from '../components/Steps'
+import FormSteps from '../components/Steps';
 import Router from 'next/router';
 export default function About() {
-    const value = useContext(AppContext);
-    useEffect(() => {
-        value.setPageState("about");
-        value.setFormState(1);
-    }, [])
-    const handlePersonalSubmit = (values) => {
-        value.setPersonalData(values);
-        Router.push('/department')
+  const value = useContext(AppContext);
+  useEffect(() => {
+    value.setPageState('about');
+    value.setFormState(1);
+
+    if (!value.state.isLoggedIn) {
+      Router.push('/form');
     }
-    return (
-        <div style={{ 
-            background: "linear-gradient( rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6) ), url(/static/bg.jpg)", 
-            backgroundSize: 'cover',
-            backgroundPosition: 'center', 
-            minHeight: '92.5vh',
-            paddingTop: '70px',
-            width: '100%' }} >
-             <FormSteps></FormSteps>
-            {
-                value.state.isLoggedIn ?
-                    <PersonalForm handleSubmit={handlePersonalSubmit} values = {
-                        value.state.personalData
-                    }></PersonalForm> :
-                    <div>
-                        <h1>Please Login</h1>
-                    </div>
-            }
+  }, []);
+  const handlePersonalSubmit = (values) => {
+    value.setPersonalData(values);
+    Router.push('/department');
+  };
+  return (
+    <div
+      style={{
+        background:
+          'linear-gradient( rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6) ), url(/static/bg.jpg)',
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        minHeight: '92.5vh',
+        paddingTop: '70px',
+        width: '100%',
+      }}
+    >
+      <FormSteps></FormSteps>
+      {value.state.isLoggedIn ? (
+        <PersonalForm
+          handleSubmit={handlePersonalSubmit}
+          values={value.state.personalData}
+        ></PersonalForm>
+      ) : (
+        <div>
+          <h1>Please Login</h1>
         </div>
-    )
+      )}
+    </div>
+  );
 }
